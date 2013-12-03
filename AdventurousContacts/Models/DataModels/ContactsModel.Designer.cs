1cc9d21953c53015a060dd928f584f5ee8b3242d
Janina <jb222qp@student.lnu.se>
2013-12-03 16:45:25 +0100

    Added db-connection

---
+++ AdventurousContacts/Models/DataModels/ContactsModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\Cepedata\Documents\GitHub\ht13-1-2-aventyrliga-kontakter\AdventurousContacts\Models\DataModels\ContactsModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
